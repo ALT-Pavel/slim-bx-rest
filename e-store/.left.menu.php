3699bcee0da49158f3763e3902fa0111a7d30e32
Crazy_Drinker <guitarlover43rus@gmail.com>
2024-09-14 16:06:29 +0300

    install bx

---
+++ e-store/.left.menu.php
@@ -0,0 +1,18 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Каталог книг", 
+		"/e-store/books/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Аффилиаты", 
+		"/e-store/affiliates/", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
